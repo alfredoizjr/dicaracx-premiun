c8439b0f5dab284480d71a8145ce93d6d3a3c239
alfredo <alfredo.alucar@gmail.com>
2017-05-31 17:30:49 -0400

    fix and scape special character in the field

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -38,14 +38,28 @@ add_action( 'admin_menu','dicaracx_premiun_add_page');
 function dicaracx_custom_settings(){
 
  // group of the settings
+
+  //first name	
   register_setting( 'dicaracx-settings-group','first_name');
+  //last name
+  register_setting( 'dicaracx-settings-group','last_name');
+   //Twhiter Handler
+  register_setting( 'dicaracx-settings-group','twiter_handler','dicaracx_sanitize_twiter_handler');
+   //Twhiter facebook
+  register_setting( 'dicaracx-settings-group','facebook_handler');
+     //Twhiter youtube
+  register_setting( 'dicaracx-settings-group','youtube_handler');
   // section we going to use
   add_settings_section( 'dicaracx-sidebar-options', 'Sidebar Option','dicaracx_sidebar_options', 'dicaracx_premiun' );
   // field we declarate and we going use in our section
-  add_settings_field( 'sidebar-name','First Name', 'dicaracx_sidebar_name', 'dicaracx_premiun', 'dicaracx-sidebar-options');
+
+  add_settings_field( 'sidebar-name','Full name', 'dicaracx_sidebar_name', 'dicaracx_premiun', 'dicaracx-sidebar-options');
+  add_settings_field( 'sidebar-twitter','Twitter handler', 'dicaracx_sidebar_twiter', 'dicaracx_premiun', 'dicaracx-sidebar-options');
+  add_settings_field( 'sidebar-facebbok','Facebook handler', 'dicaracx_sidebar_facebook', 'dicaracx_premiun', 'dicaracx-sidebar-options');
+  add_settings_field( 'sidebar-youtube','Youtube handler', 'dicaracx_sidebar_youtube', 'dicaracx_premiun', 'dicaracx-sidebar-options');
 }
 
-// section of sidebar
+// section of sidebar full name
 function dicaracx_sidebar_options(){
   
   echo 'Customize Your sidebar information';
@@ -53,10 +67,52 @@ function dicaracx_sidebar_options(){
 
 // field of sidebar
 function dicaracx_sidebar_name(){
+ //first name field	
   $firstname = esc_attr( get_option( 'first_name' ));
   echo '<input type="text" name="first_name" value="'.$firstname.'" placeholder="First Name">';
+
+  //last name field
+  $lastname = esc_attr( get_option( 'last_name' ));
+  echo '<input type="text" name="last_name" value="'.$lastname.'" placeholder="last name">';
 }
 
+// twitter
+
+function dicaracx_sidebar_twiter(){
+
+//twiter field
+  $twiter = esc_attr( get_option( 'twiter_handler' ));
+  echo '<input type="text" name="twiter_handler" value="'.$twiter.'" placeholder="Twitter handler">';
+
+}
+
+
+function dicaracx_sidebar_facebook(){
+
+//twiter facebook
+  $facebook = esc_attr( get_option( 'facebook_handler' ));
+  echo '<input type="text" name="facebook_handler" value="'.$facebook.'" placeholder="facebook handler">';
+
+}
+
+function dicaracx_sidebar_youtube(){
+
+//twiter youtube
+  $youtube = esc_attr( get_option( 'youtube_handler' ));
+  echo '<input type="text" name="youtube_handler" value="'.$youtube.'" placeholder="youtube handler">';
+
+}
+
+
+//Sanitazite setting
+
+function dicaracx_sanitize_twiter_handler($input){
+ // we try clear special character it s in the field	
+   $output = sanitize_text_field( $input );
+ // we going scape to the @ character
+   $output = str_replace('@', '', $output);
+   return $output;
+} 
 
 
 function dicaracx_premiun_create_page(){
